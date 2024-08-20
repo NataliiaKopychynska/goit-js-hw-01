c6e9255d0788b6f2b95b5cbb623cba2e86673574
Nataliia <nkopichinckaya@gmail.com>
2024-08-20 12:01:55 +0200

    1true

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+  const contentWidthg = Number.parseInt(content);
+  const paddingWidth = Number.parseInt(padding);
+  const borderWidth = Number.parseInt(border);
+
+  const value = contentWidthg + paddingWidth * 2 + borderWidth * 2;
+  return value;
+}
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
