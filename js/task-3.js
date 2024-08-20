fa2851328b8edb2ecc4e449986e7c580a6f73106
Nataliia <nkopichinckaya@gmail.com>
2024-08-20 19:16:01 +0200

    1true

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 'use strict';
 function getElementWidth(content, padding, border) {
-  const contentWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding);
-  const borderWidth = Number.parseFloat(border);
+  const contentWidthg = Number.parseInt(content);
+  const paddingWidth = Number.parseInt(padding);
+  const borderWidth = Number.parseInt(border);
 
   const value = contentWidthg + paddingWidth * 2 + borderWidth * 2;
   return value;
