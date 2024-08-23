575ed1ae31c67cda22cd5172762fc314955a73e4
Nataliia <nkopichinckaya@gmail.com>
2024-08-23 12:27:24 +0200

    1true

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,4 @@
 'use strict';
-// function getElementWidth(content, padding, border) {
-//   const contentWidthg = Number.parseInt(content);
-//   const paddingWidth = Number.parseInt(padding);
-//   const borderWidth = Number.parseInt(border);
-
-//   const value = contentWidthg + paddingWidth * 2 + borderWidth * 2;
-//   return value;
-// }
-
-// console.log(getElementWidth('50px', '8px', '4px')); // 74
-// console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-// console.log(getElementWidth('200px', '0px', '0px')); // 200
 
 // не знайшла цього в конспекті чи можливо пропустила, підскажіть де шукати?
 function getElementWidth(content, padding, border) {
